8ec2598640312d1c4b2e6e90fd723a346b284e20
zachsirera <z.sirera@gmail.com>
2020-10-16 17:35:12 -0600

    added a line to get 10% of tweet_data to speed up page loading

--- app.py
+++ app.py
@@ -4,6 +4,8 @@
 # import the necessary libraries
 from flask import Flask, render_template
 
+import random
+
 # import files from the project directory
 import analyze
 import api
@@ -16,9 +18,10 @@ def home():
 	aggregated_tweets = api.get_aggregated_tweets()
 	# commenting this out for now, since it is much larger than we need. May want to random sample to reduce the amount of time it takes to load
 	tweet_data = api.get_tweet_data()
+	reduced_tweet_data = random.sample(tweet_data, int(0.1 * len(tweet_data)))
 	# tweet_data = []
 
-	return render_template('home.html', events=events, aggregated_tweets=aggregated_tweets, tweet_data=tweet_data)
+	return render_template('home.html', events=events, aggregated_tweets=aggregated_tweets, tweet_data=reduced_tweet_data)
 
 if __name__ == '__main__':
 	app.run()
