fcb0f5b65ad0c8276aa72b531cb204fb267c03c1
zachsirera <z.sirera@gmail.com>
2020-10-16 17:29:29 -0600

    added basic dynamic scatter plot that responds to user selection in header. doesn't update x_attribute though, need another select for that

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ def home():
 	events = api.get_events()
 	aggregated_tweets = api.get_aggregated_tweets()
 	# commenting this out for now, since it is much larger than we need. May want to random sample to reduce the amount of time it takes to load
-	# tweet_data = api.get_tweet_data()
-	tweet_data = []
+	tweet_data = api.get_tweet_data()
+	# tweet_data = []
 
 	return render_template('home.html', events=events, aggregated_tweets=aggregated_tweets, tweet_data=tweet_data)
 
